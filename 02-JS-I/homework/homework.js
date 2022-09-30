93b1fde40312b9ead26451caafa607329bf876c2
GIANCARLO2963 <juancarlos296376@gmail.com>
2022-09-30 08:24:40 -0400

    hora 823

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,10 +28,20 @@ son iguales y luego de eso a la constante llamada nuevaResta se le asigna el val
 const nuevaMultiplicacion = 10 * 4 === 40 ;/* en la linea 63 del archivo JSI.test.js dice que nuevaMultiplicacion
 deberia ser un booleano luego del comando npm test JSI.test.js el resultado es que he pasado 5 y me quedan 30*/
 
-// Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+//EJERCICIO_NUMERO_6 Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 5 === 1;
+/* 21 dividido en 5 ¿Cuantas veces cabe el 5 dentro del 21? Respuesta :cabe 4 veces luego
+¿Cuanto es 4 multiplicado por 5? Respuesta: 20 
+Ahora ¿Cuanto queda si a 21 le restamos 20? Respuesta:sobra 1 unidad. 
+Esto es lo que se conoce como el resto. 
+Podriamos seguir dividiendo y obtener una division precisa y con numeros decimales pero el calculo que
+estamos haciendo aqui solo es el "MOD" , es decir , el simbolo % y por lo tanto 
+21 % 5 ===1  nada tiene que hacer aqui el llamado puntero NULL y por eso se borra y se cambia por 1*/
+/*en la linea 69 del archivo JSI.test.js dice que nuevaModulo
+deberia ser un booleano luego del comando npm test JSI.test.js 
+Resultado es que he pasado 6 y me quedan 29*/
 
-
+/*EJERCICIO_NUMERO_7*/
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
